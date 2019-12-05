73bd0d2e1c5e74ce83b419d6e140667869585017
pcjbird <pcjbird@hotmail.com>
2019-12-05 11:20:31 +0800

    close button

--- apps/A_Nice_Morphing_Clock_for_Me/website/jp/js/videoplayer.js
+++ apps/A_Nice_Morphing_Clock_for_Me/website/jp/js/videoplayer.js
@@ -4,7 +4,7 @@ function playVideo(videoUrl)
     document.getElementById("video_overlay").style.display="block";
     document.getElementById("video_holder").style.display="block";
     document.getElementById("video_loader").style.display="block";
-    document.getElementById("video_holder").innerHTML = "<video autoplay loop controls id=\"video\" class=\"video\"><source src=\""+videoUrl+"\" type=\"video/mp4\"></video>";
+    document.getElementById("video_holder").innerHTML = "<div class=\"video_close\" onclick=\"closeVideo()\"></div><video autoplay loop controls id=\"video\" class=\"video\"><source src=\""+videoUrl+"\" type=\"video/mp4\"></video>";
 }
 
 function closeVideo()
